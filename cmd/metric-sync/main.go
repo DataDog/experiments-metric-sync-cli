e8e226f520fa2386fbdef82ef53628c0734f4688
Danyal Khan <danyal.khan@datadoghq.com>
2026-06-26 14:34:35 -0400

    Add CLI main package to release build

---
+++ cmd/metric-sync/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+
+	"github.com/DataDog/datadog-experiment-metric-sync-cli/internal/cli"
+)
+
+var (
+	version = "dev"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
+func main() {
+	os.Exit(cli.Run(os.Args[1:], cli.VersionInfo{
+		Version: version,
+		Commit:  commit,
+		Date:    date,
+	}))
+}
